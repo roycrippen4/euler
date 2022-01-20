2947665962d26d04a8e8da89dc881bf58b5566b4
roycrippen4 <roycrippen4@gmail.com>
2022-01-20 14:28:40 -0600

    stuff

---
+++ p003.py
@@ -0,0 +1,12 @@
+# P003
+# The prime factors of 13195 are 5, 7, 13, and 29
+# What is the largest prime factor of the number 600851475143
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
+
