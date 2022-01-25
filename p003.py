15e57ef6270b903f7825c406419b0118bbcbd749
roycrippen4 <roycrippen4@gmail.com>
2022-01-25 14:43:01 -0600

    commit

--- p003.py
+++ p003.py
@@ -1,11 +1,20 @@
 # P003
-# The prime factors of 13195 are 5, 7, 13, and 29
-# What is the largest prime factor of the number 600851475143
+# The prime factors of 13_195 are 5, 7, 13, and 29
+# What is the largest prime factor of the number 600_851_475_143
+import math
 import utilities.is_prime
 
-
 def main():
-    utilities.is_prime.is_prime(5)
+    divisors = []
+    n = 13_195
+    round_sqrt_n = round(math.sqrt(13_195))
+
+    for num in range(2, round_sqrt_n):
+        if n % num == 0:
+            divisors.append(num)
+    for divisor in divisors:
+
+    print(divisors)
 
 
 if __name__ == "__main__":
