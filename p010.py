6ad6a738aa4835e6483373ce2b2ff1399c64c6e1
roycrippen4 <roycrippen4@gmail.com>
2022-03-10 13:12:07 -0600

    commit

---
+++ p010.py
@@ -0,0 +1,18 @@
+# P010
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+from sympy import isprime
+
+
+def main():
+    summation = 2 + 3 + 5
+    for i in range(7, 2_000_000, 2):
+        if i % 3 == 0 or i % 5 == 0:
+            continue
+        if isprime(i):
+            summation += i
+    print(summation)
+
+
+if __name__ == "__main__":
+    main()
