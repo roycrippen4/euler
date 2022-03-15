783ece61e1efed95eca9c266893e6c61b6bd1917
roycrippen4 <roycrippen4@gmail.com>
2022-03-15 16:23:59 -0500

    commit

--- p011.py
+++ p011.py
@@ -1,8 +1,41 @@
 # P011 In the given 20x20 grid, what is the greatest product of four adjacent numbers in the same direction (up,
 # down, left, right, or diagonally).
 
-from utilities import p011_get_data, p011_get_south, p011_get_east, \
-    p011_get_southwest, p011_get_southeast
+def p011_get_data():
+    grid = []
+    with open('/Users/roycrippen/dev/python/euler/supplementary_files/p011_grid') as file:
+        for line in file:
+            split_line = [int(i) for i in line.split()]
+            grid.append(split_line)
+    return grid
+
+
+def p011_get_south(x, y, grid):
+    current_product = 0
+    if y < 17:
+        current_product = grid[y][x] * grid[y + 1][x] * grid[y + 2][x] * grid[y + 3][x]
+    return current_product
+
+
+def p011_get_east(x, y, grid):
+    current_product = 0
+    if x < 17:
+        current_product = grid[y][x] * grid[y][x + 1] * grid[y][x + 2] * grid[y][x + 3]
+    return current_product
+
+
+def p011_get_southeast(x, y, grid):
+    current_product = 0
+    if x < 17 and y < 17:
+        current_product = grid[y][x] * grid[y + 1][x + 1] * grid[y + 2][x + 2] * grid[y + 3][x + 3]
+    return current_product
+
+
+def p011_get_southwest(x, y, grid):
+    current_product = 0
+    if x > 2 and y < 17:
+        current_product = grid[y][x] * grid[y + 1][x - 1] * grid[y + 2][x - 2] * grid[y + 3][x - 3]
+    return current_product
 
 
 def main():
--- utilities.py
+++ utilities.py
@@ -74,43 +74,6 @@ def get_digit_list():
     # return digits
 
 
-def p011_get_data():
-    grid = []
-    with open('/Users/roycrippen/dev/python/euler/supplementary_files/p011_grid') as file:
-        for line in file:
-            split_line = [int(i) for i in line.split()]
-            grid.append(split_line)
-    return grid
-
-
-def p011_get_south(x, y, grid):
-    current_product = 0
-    if y < 17:
-        current_product = grid[y][x] * grid[y + 1][x] * grid[y + 2][x] * grid[y + 3][x]
-    return current_product
-
-
-def p011_get_east(x, y, grid):
-    current_product = 0
-    if x < 17:
-        current_product = grid[y][x] * grid[y][x + 1] * grid[y][x + 2] * grid[y][x + 3]
-    return current_product
-
-
-def p011_get_southeast(x, y, grid):
-    current_product = 0
-    if x < 17 and y < 17:
-        current_product = grid[y][x] * grid[y + 1][x + 1] * grid[y + 2][x + 2] * grid[y + 3][x + 3]
-    return current_product
-
-
-def p011_get_southwest(x, y, grid):
-    current_product = 0
-    if x > 2 and y < 17:
-        current_product = grid[y][x] * grid[y + 1][x - 1] * grid[y + 2][x - 2] * grid[y + 3][x - 3]
-    return current_product
-
-
 def is_triplet(a: int, b: int):
     return a < b and sqrt(a * a + b * b) % 1 == 0
 
