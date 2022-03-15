f6d83e5f3d36e6b7eaa52d3eda52128df5a44bee
roycrippen4 <roycrippen4@gmail.com>
2022-03-15 16:43:05 -0500

    commit

--- p011.py
+++ p011.py
@@ -11,43 +11,31 @@ def p011_get_data():
 
 
 def p011_get_south(x, y, grid):
-    current_product = 0
-    if y < 17:
-        current_product = grid[y][x] * grid[y + 1][x] * grid[y + 2][x] * grid[y + 3][x]
-    return current_product
+    return grid[y][x] * grid[y + 1][x] * grid[y + 2][x] * grid[y + 3][x] if y < 17 else 0
 
 
 def p011_get_east(x, y, grid):
-    current_product = 0
-    if x < 17:
-        current_product = grid[y][x] * grid[y][x + 1] * grid[y][x + 2] * grid[y][x + 3]
-    return current_product
+    return grid[y][x] * grid[y][x + 1] * grid[y][x + 2] * grid[y][x + 3] if x < 17 else 0
 
 
 def p011_get_southeast(x, y, grid):
-    current_product = 0
-    if x < 17 and y < 17:
-        current_product = grid[y][x] * grid[y + 1][x + 1] * grid[y + 2][x + 2] * grid[y + 3][x + 3]
-    return current_product
+    return grid[y][x] * grid[y + 1][x + 1] * grid[y + 2][x + 2] * grid[y + 3][x + 3] if x < 17 and y < 17 else 0
 
 
 def p011_get_southwest(x, y, grid):
-    current_product = 0
-    if x > 2 and y < 17:
-        current_product = grid[y][x] * grid[y + 1][x - 1] * grid[y + 2][x - 2] * grid[y + 3][x - 3]
-    return current_product
+    return grid[y][x] * grid[y + 1][x - 1] * grid[y + 2][x - 2] * grid[y + 3][x - 3] if x > 2 and y < 17 else 0
 
 
 def main():
     grid = p011_get_data()
-    products = []
+    current_max = 0
     for x in range(0, len(grid)):
         for y in range(0, len(grid)):
-            products.append(p011_get_south(x, y, grid))
-            products.append(p011_get_east(x, y, grid))
-            products.append(p011_get_southwest(x, y, grid))
-            products.append(p011_get_southeast(x, y, grid))
-    print(max(products))
+            current_max = max(current_max, p011_get_south(x, y, grid))
+            current_max = max(current_max, p011_get_east(x, y, grid))
+            current_max = max(current_max, p011_get_southwest(x, y, grid))
+            current_max = max(current_max, p011_get_southeast(x, y, grid))
+    print(current_max)
 
 
 if __name__ == "__main__":
