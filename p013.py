7de66fe0638705a4d187a8613f6133cd88eec111
roycrippen4 <roycrippen4@gmail.com>
2022-03-16 03:11:13 -0500

    moved p001-p010 to new directory

---
+++ p013.py
@@ -0,0 +1,23 @@
+# P013
+# Work out the first ten digits of the sum of the given one-hundred 50-digit numbers.
+
+
+def main():
+    big_num = str(p013_get_sum())
+    answer = ''
+    for i in range(0, 10):
+        answer += str(big_num[i])
+    print(answer)  # 5537376230
+
+
+def p013_get_sum():
+    with open('/Users/roycrippen/dev/python/euler/supplementary_files/p013_digits.txt') as file:
+        big_num = 0
+        for line in file:
+            split_line = [int(i) for i in line.split()]
+            big_num += split_line[0]
+    return big_num
+
+
+if __name__ == "__main__":
+    main()
