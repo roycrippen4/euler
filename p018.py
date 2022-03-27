057ee5f598abe59a67c13530fc9a6a594c8d0acb
roycrippen4 <roycrippen4@gmail.com>
2022-03-27 12:42:30 -0500

    commit

--- p018.py
+++ p018.py
@@ -1,21 +1,23 @@
 # P018
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from
+# top to bottom is 23.
+#
+# `3
+# `7  4
+#  2 `4  6
+#  8  5 `9  3
 
+# That is, 3 + 7 + 4 + 9 = 23.
+
+# Find the maximum total from top to bottom of the given triangle.
 
 def main():
     num_triangle = p018_data_prep()
     num_triangle.reverse()
-    print_triangle(num_triangle)
     for row in range(1, len(num_triangle)):
         for col in range(0, len(num_triangle[row])):
             num_triangle[row][col] += max(num_triangle[row - 1][col], num_triangle[row - 1][col + 1])
-        # print_triangle(num_triangle)
-    print(num_triangle[len(num_triangle) - 1][0])  # 1074
-
-
-def print_triangle(xss):
-    for xs in xss:
-        print(xs)
-    print()
+    print('ans =', num_triangle[len(num_triangle) - 1][0])  # 1074
 
 
 def p018_data_prep():
